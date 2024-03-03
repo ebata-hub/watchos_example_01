21ec35a0cb5a628dab09d3c77f503305ae404c40
ebata <ebata@me.com>
2024-03-03 23:14:36 +0900

    update

---
+++ Example01 Watch App/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Example01 Watch App
+//
+//  Created by Masamichi Ebata on 2024/03/03.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
---
+++ Example01 Watch App/Note.swift
@@ -0,0 +1,8 @@
+//
+//  Note.swift
+//  Example01 Watch App
+//
+//  Created by Masamichi Ebata on 2024/03/03.
+//
+
+import Foundation
